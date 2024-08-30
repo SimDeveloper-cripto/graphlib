6df928ae108606f46f1430e5e6d2788ae129f814
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-08-30 09:17:54 +0200

    cleanup and added encapsulation.

--- graph.py
+++ graph.py
@@ -4,106 +4,106 @@ from color import Color
 # NON-ORIENTED GRAPH IMPLEMENTED BY MATRIX
 class Graph:
     def __init__(self):
-        self.nodes  = []       # List to hold nodes
-        self.matrix = []       # Adjacency Matrix
-        self.node_colors = {}  # Dictionary to track colors of nodes
+        self.__nodes  = []       # List to hold nodes
+        self.__matrix = []       # Adjacency Matrix
+        self.__node_colors = {}  # Dictionary to track colors of nodes
 
     def Init(self):
-        for node in self.node_colors:
-            self.node_colors[node] = Color.WHITE
+        for node in self.__node_colors:
+            self.__node_colors[node] = Color.WHITE
 
-    def _resize_matrix(self, new_size):
-        old_size  = len(self.matrix)
+    def __resize_matrix(self, new_size):
+        old_size  = len(self.__matrix)
         curr_size = new_size - old_size
 
         # Expand existing rows
-        for row in self.matrix:
+        for row in self.__matrix:
             row.extend([0] * curr_size)
 
         # Add new rows
         for _ in range(curr_size):
-            self.matrix.append([0] * new_size)
+            self.__matrix.append([0] * new_size)
 
     def addNode(self, node):
-        if node not in self.nodes:
-            self.nodes.append(node)
-            self._resize_matrix(len(self.nodes))
-            self.node_colors[node] = Color.WHITE
+        if node not in self.__nodes:
+            self.__nodes.append(node)
+            self.__resize_matrix(len(self.__nodes))
+            self.__node_colors[node] = Color.WHITE
 
     def addEdge(self, node1, node2):
-        if node1 not in self.nodes:
+        if node1 not in self.__nodes:
             self.addNode(node1)
-        if node2 not in self.nodes:
+        if node2 not in self.__nodes:
             self.addNode(node2)
 
-        index1 = self.nodes.index(node1)
-        index2 = self.nodes.index(node2)
-        self.matrix[index1][index2] = 1
-        self.matrix[index2][index1] = 1
+        index1 = self.__nodes.index(node1)
+        index2 = self.__nodes.index(node2)
+        self.__matrix[index1][index2] = 1
+        self.__matrix[index2][index1] = 1
 
     def show(self):
-        col_width = max(len(str(node)) for node in self.nodes) + 2
+        col_width = max(len(str(node)) for node in self.__nodes) + 2
 
         print(" " * (col_width + 1) + "|", end="")
-        for node in self.nodes:
+        for node in self.__nodes:
             print(f" {str(node):<{col_width}}", end="")
         print()
 
-        print(" " * (col_width + 1) + "+" + "-" * ((col_width + 1) * len(self.nodes)))
-        for i, row in enumerate(self.matrix):
-            print(f"{str(self.nodes[i]):<{col_width}} |", end="")
+        print(" " * (col_width + 1) + "+" + "-" * ((col_width + 1) * len(self.__nodes)))
+        for i, row in enumerate(self.__matrix):
+            print(f"{str(self.__nodes[i]):<{col_width}} |", end="")
             for val in row:
                 print(f" {val:<{col_width}}", end="")
             print()
 
-    def DfsVisit(self, node, map_func=None, fold_func=None, acc=None):
+    def __DfsVisit(self, node, map_func=None, fold_func=None, acc=None):
         if map_func is not None:
-            map_func(node)  # APPLY MAP
+            map_func(node)
 
         if fold_func is not None:
-            fold_func(acc, node)  # APPLY FOLD
+            acc = fold_func(acc, node)
 
-        self.node_colors[node] = Color.GRAY
+        self.__node_colors[node] = Color.GRAY
 
-        index = self.nodes.index(node)
-        for i, connected in enumerate(self.matrix[index]):
-            if connected and self.node_colors[self.nodes[i]] == Color.WHITE:
-                acc = self.DfsVisit(self.nodes[i], map_func, fold_func, acc)
+        index = self.__nodes.index(node)
+        for i, connected in enumerate(self.__matrix[index]):
+            if connected and self.__node_colors[self.__nodes[i]] == Color.WHITE:
+                acc = self.__DfsVisit(self.__nodes[i], map_func, fold_func, acc)
 
-        self.node_colors[node] = Color.BLACK
+        self.__node_colors[node] = Color.BLACK
         return acc
 
     # DFS WITH BOTH MAP AND FOLD
     def Dfs(self, map_func=None, fold_func=None, acc=None):
         self.Init()
-        for node in self.nodes:
-            if self.node_colors[node] == Color.WHITE:
-                acc = self.DfsVisit(node, map_func, fold_func, acc)
+        for node in self.__nodes:
+            if self.__node_colors[node] == Color.WHITE:
+                acc = self.__DfsVisit(node, map_func, fold_func, acc)
 
         return acc
 
     # BFS WITH BOTH MAP AND FOLD
     def Bfs(self, start_node, map_func=None, fold_func=None, acc=None):
-        if start_node not in self.nodes:
+        if start_node not in self.__nodes:
             return acc
 
         self.Init()
         queue = [start_node]
-        self.node_colors[start_node] = Color.GRAY
+        self.__node_colors[start_node] = Color.GRAY
 
         while queue:
             curr = queue.pop(0)
             if map_func is not None:
-                map_func(curr)  # APPLY MAP
+                map_func(curr)
 
             if fold_func is not None:
-                acc = fold_func(acc, curr)  # APPLY FOLD
+                acc = fold_func(acc, curr)
 
-            index = self.nodes.index(curr)
-            for i, connected in enumerate(self.matrix[index]):
-                if connected and self.node_colors[self.nodes[i]] == Color.WHITE:
-                    queue.append(self.nodes[i])
-                    self.node_colors[self.nodes[i]] = Color.GRAY
+            index = self.__nodes.index(curr)
+            for i, connected in enumerate(self.__matrix[index]):
+                if connected and self.__node_colors[self.__nodes[i]] == Color.WHITE:
+                    queue.append(self.__nodes[i])
+                    self.__node_colors[self.__nodes[i]] = Color.GRAY
 
-            self.node_colors[curr] = Color.BLACK
+            self.__node_colors[curr] = Color.BLACK
         return acc
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graph import Graph
 
 
-# Example Map() function
+# Example Map() function to print visited nodes
 def map_visit(node):
     print(f"{node}", end=" ")
 
@@ -13,44 +13,50 @@ def fold_sum(acc, node):
     return acc
 
 
-# Example Fold() function to how many nodes got visited
+# Example Fold() function to count how many nodes got visited
 def fold_count(acc, node):
     return acc + 1
 
 
-# Not-Oriented Graph Example
-g = Graph()
+def run_dev_test():
+    g = Graph()
 
-g.addNode(1)
-g.addNode(2)
-g.addNode(3)
-g.addNode("A")
+    g.addNode(1)
+    g.addNode(2)
+    g.addNode(3)
+    g.addNode("A")
 
-g.addEdge(1, 2)
-g.addEdge(2, 3)
-g.addEdge(3, "A")
+    g.addEdge(1, 2)
+    g.addEdge(2, 3)
+    g.addEdge(3, "A")
 
-# [START DEVELOPER TEST]
+    # [START]
 
-g.show()  # PRINT GRAPH STRUCTURE
+    g.show()  # PRINT GRAPH STRUCTURE
 
-print("\n--- MAP DFS:")
-g.Dfs(map_func=map_visit)  # PERFORM DFS USING MAP
+    print("\n--- MAP DFS:")
+    g.Dfs(map_func=map_visit)  # PERFORM DFS USING MAP
 
-print("\n\n--- FOLD DFS:")
-result = g.Dfs(fold_func=fold_sum, acc=0)  # PERFORM DFS USING FOLD
-print(f"\t--- RESULT VALUE: {result}")
+    print("\n--- FOLD DFS:")
+    result = g.Dfs(fold_func=fold_sum, acc=0)  # PERFORM DFS USING FOLD
+    print(f"\t--- EMPTY MAP RESULT: {result}")
 
-print("\n--- FOLD BFS STARTING FROM '1':")
-node_count = g.Bfs(1, fold_func=fold_count, acc=0)  # PERFORM BFS USING FOLD
-print(f"\t--- NUMBER OF VISITED NODES: {node_count}")
+    print("\n--- MAP BFS STARTING FROM '1':")
+    g.Bfs(1, map_func=map_visit)  # PERFORM BFS USING MAP
 
-# [END DEVELOPER TEST]
+    print("\n--- FOLD BFS STARTING FROM '1':")
+    node_count = g.Bfs(1, fold_func=fold_count, acc=0)  # PERFORM BFS USING FOLD
+    print(f"\t--- NUMBER OF VISITED NODES: {node_count}")
 
-# TODO: 1. ADD ENCAPSULATION
-# TODO: 2. CYCLIC TEST
-# TODO: 3. MINIMUM PATHS
-# TODO: 4. TOPOLOGICAL SORT
-# TODO: 5. SCC
-# TODO: 6. ADD GRAPHICS
-# TODO: 7. COMPLETE ONE ASD EXAM EXERCISE
+    # [END]
+
+
+if __name__ == "__main__":
+    run_dev_test()
+
+# TODO: 1. CYCLIC TEST
+# TODO: 2. MINIMUM PATHS
+# TODO: 3. TOPOLOGICAL SORT
+# TODO: 4. SCC
+# TODO: 5. ADD GRAPHICS
+# TODO: 6. COMPLETE ONE ASD EXAM EXERCISE
