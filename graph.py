57ed2ecda83ce490d82f88abe68bde9606f6e36d
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-09-01 20:06:02 +0200

    managed to add graphics and minimum path calculation.

--- graph.py
+++ graph.py
@@ -1,4 +1,7 @@
 from color import Color
+from collections import deque
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 # NON-ORIENTED GRAPH IMPLEMENTED BY MATRIX
@@ -8,7 +11,7 @@ class Graph:
         self.__matrix = []       # Adjacency Matrix
         self.__node_colors = {}  # Dictionary to track colors of nodes
 
-    def Init(self):
+    def __Init(self):
         for node in self.__node_colors:
             self.__node_colors[node] = Color.WHITE
 
@@ -41,6 +44,12 @@ class Graph:
         self.__matrix[index1][index2] = 1
         self.__matrix[index2][index1] = 1
 
+    def getNodes(self):
+        return self.__nodes
+
+    def getCurrentStateOfColors(self):
+        return self.__node_colors
+
     def show(self):
         col_width = max(len(str(node)) for node in self.__nodes) + 2
 
@@ -55,6 +64,66 @@ class Graph:
             for val in row:
                 print(f" {val:<{col_width}}", end="")
             print()
+
+    def visualize(self):
+        # USING NETWORKX API
+        g = nx.Graph()
+
+        for node in self.__nodes:
+            g.add_node(node)
+
+        for i in range(len(self.__nodes)):
+            for j in range(len(self.__nodes)):
+                if self.__matrix[i][j] == 1:
+                    g.add_edge(self.__nodes[i], self.__nodes[j])
+
+        color_map = {
+            Color.WHITE: 'white',
+            Color.GRAY:  'gray',
+            Color.BLACK: 'black',
+        }
+        b_f_color_map = {
+            Color.WHITE: 'black',
+            Color.GRAY:  'black',
+            Color.BLACK: 'white'
+        }
+        edge_color = 'gray'
+
+        node_colors = [color_map[self.__node_colors[node]] for node in self.__nodes]
+        font_colors = [b_f_color_map[self.__node_colors[node]] for node in self.__nodes]
+
+        # DRAW USING __Matplotlib__
+        pos = nx.spring_layout(g)
+        fig, ax = plt.subplots()
+
+        fig.canvas.manager.set_window_title('Graph Visualizer')
+
+        nx.draw(
+            g,
+            pos,
+            with_labels = True,
+            node_color  = node_colors,
+            edge_color  = edge_color,
+            node_size   = 2000,
+            font_size   = 15,
+            font_weight = 'bold',
+            edgecolors = font_colors
+        )
+
+        for node, (x, y) in pos.items():
+            ax.text(
+                x, y,
+                s          = node,
+                bbox       = dict(facecolor=node_colors[self.__nodes.index(node)], edgecolor=font_colors[self.__nodes.index(node)], boxstyle='round,pad=0.3'),
+                color      = b_f_color_map[self.__node_colors[node]],
+                fontsize   = 15,
+                fontweight = 'bold',
+                ha         = 'center',
+                va         = 'center'
+            )
+
+        # ax.set_title("Graph Visualizer", fontsize=20, fontweight='bold')
+        plt.show()
 
     def __DfsVisit(self, node, map_func=None, fold_func=None, acc=None):
         if map_func is not None:
@@ -75,7 +144,7 @@ class Graph:
 
     # DFS WITH BOTH MAP AND FOLD
     def Dfs(self, map_func=None, fold_func=None, acc=None):
-        self.Init()
+        self.__Init()
         for node in self.__nodes:
             if self.__node_colors[node] == Color.WHITE:
                 acc = self.__DfsVisit(node, map_func, fold_func, acc)
@@ -87,7 +156,7 @@ class Graph:
         if start_node not in self.__nodes:
             return acc
 
-        self.Init()
+        self.__Init()
         queue = [start_node]
         self.__node_colors[start_node] = Color.GRAY
 
@@ -107,3 +176,27 @@ class Graph:
 
             self.__node_colors[curr] = Color.BLACK
         return acc
+
+    # GET MINUMUM PATH BETWEEN TWO NODES
+    def getMinimumPath(self, start_node, end_node):
+        if start_node not in self.__nodes or end_node not in self.__nodes:
+            return None
+
+        self.__Init()
+        queue = deque([(start_node, [start_node])])  # Tuple of (curr_node, path)
+        self.__node_colors[start_node] = Color.GRAY
+
+        while queue:
+            curr, path = queue.popleft()
+            if curr == end_node:
+                return path
+
+            index = self.__nodes.index(curr)
+            for i, connected in enumerate(self.__matrix[index]):
+                node_v = self.__nodes[i]
+                if connected and self.__node_colors[node_v] == Color.WHITE:
+                    self.__node_colors[node_v] = Color.GRAY
+                    queue.append((node_v, path + [node_v]))
+
+            self.__node_colors[curr] = Color.BLACK
+        return None
--- main.py
+++ main.py
@@ -21,19 +21,25 @@ def fold_count(acc, node):
 def run_dev_test():
     g = Graph()
 
-    g.addNode(1)
-    g.addNode(2)
-    g.addNode(3)
     g.addNode("A")
+    g.addNode("B")
+    g.addNode("C")
+    g.addNode("D")
+    g.addNode("E")
 
-    g.addEdge(1, 2)
-    g.addEdge(2, 3)
-    g.addEdge(3, "A")
+    g.addEdge("A", "B")
+    g.addEdge("A", "C")
+    g.addEdge("B", "D")
+    g.addEdge("C", "D")
+    g.addEdge("D", "E")
 
     # [START]
 
-    g.show()  # PRINT GRAPH STRUCTURE
+    # g.show()     # PRINT GRAPH STRUCTURE
+    g.visualize()  # VISUALIZE THE GRAPH
 
+
+'''
     print("\n--- MAP DFS:")
     g.Dfs(map_func=map_visit)  # PERFORM DFS USING MAP
 
@@ -41,12 +47,16 @@ def run_dev_test():
     result = g.Dfs(fold_func=fold_sum, acc=0)  # PERFORM DFS USING FOLD
     print(f"\t--- EMPTY MAP RESULT: {result}")
 
-    print("\n--- MAP BFS STARTING FROM '1':")
-    g.Bfs(1, map_func=map_visit)  # PERFORM BFS USING MAP
+    print("\n--- MAP BFS STARTING FROM 'A':")
+    g.Bfs("A", map_func=map_visit)  # PERFORM BFS USING MAP
 
-    print("\n--- FOLD BFS STARTING FROM '1':")
-    node_count = g.Bfs(1, fold_func=fold_count, acc=0)  # PERFORM BFS USING FOLD
+    print("\n--- FOLD BFS STARTING FROM 'A':")
+    node_count = g.Bfs("A", fold_func=fold_count, acc=0)  # PERFORM BFS USING FOLD
     print(f"\t--- NUMBER OF VISITED NODES: {node_count}")
+
+    min_path = g.getMinimumPath("A", "E")
+    print("\nMINIMUM PATH BETWEEN A & E:", min_path)
+'''
 
     # [END]
 
@@ -54,9 +64,6 @@ def run_dev_test():
 if __name__ == "__main__":
     run_dev_test()
 
-# TODO: 1. CYCLIC TEST
-# TODO: 2. MINIMUM PATHS
-# TODO: 3. TOPOLOGICAL SORT
-# TODO: 4. SCC
-# TODO: 5. ADD GRAPHICS
-# TODO: 6. COMPLETE ONE ASD EXAM EXERCISE
+# TODO: 1. TOPOLOGICAL SORT
+# TODO: 2. SCC
+# TODO: 3. COMPLETE ONE ASD EXAM EXERCISE
